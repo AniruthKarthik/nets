333fd3c4a2e0efcbb68a3eefb36ba109e4054a06
Krishanth K <krishanth.k2006@gmail.com>
2025-12-23 23:40:56 +0530

    added quicksort algo

--- cpp/CpuStrategy.hpp
+++ cpp/CpuStrategy.hpp
@@ -33,6 +33,60 @@ inline vector<Move> generateMoves(const Board &board, pair<int, int> lastMovedTi
   return moves;
 }
 
+// Helper function to define the importance of each tile type
+inline int getTileTypePriority(TileType type) {
+    switch (type) {
+        case T_JUNCTION: return 4;
+        case CORNER:     return 3;
+        case STRAIGHT:   return 2;
+        case PC:         return 1;
+        default:         return 0;
+    }
+}
+
+// Swaps two Move objects
+inline void swapMoves(Move &a, Move &b) {
+    Move temp = a;
+    a = b;
+    b = temp;
+}
+
+// Partition function for Quick Sort
+inline int quickSort_partition(vector<Move> &moves, int low, int high, const Board &board) {
+    Move pivot = moves[high];
+    TileType pivotType = board.at(pivot.x, pivot.y).type;
+    int pivotPriority = getTileTypePriority(pivotType);
+    
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        TileType jType = board.at(moves[j].x, moves[j].y).type;
+        // Sort in descending order of priority
+        if (getTileTypePriority(jType) > pivotPriority) {
+            i++;
+            swapMoves(moves[i], moves[j]);
+        }
+    }
+    swapMoves(moves[i + 1], moves[high]);
+    return (i + 1);
+}
+
+// Recursive Quick Sort function
+inline void quickSort_recursive(vector<Move> &moves, int low, int high, const Board &board) {
+    if (low < high) {
+        int pi = quickSort_partition(moves, low, high, board);
+        quickSort_recursive(moves, low, pi - 1, board);
+        quickSort_recursive(moves, pi + 1, high, board);
+    }
+}
+
+// Main function to initiate Quick Sort
+inline void sortMoves(vector<Move> &moves, const Board &board) {
+    if (!moves.empty()) {
+        quickSort_recursive(moves, 0, moves.size() - 1, board);
+    }
+}
+
 inline int evaluateBoard(const Board &board) {
   Graph graph = buildGraph(board);
   int looseEnds = countLooseEnds(board);
@@ -50,6 +104,8 @@ inline Move chooseBestMove(const Board &board, pair<int, int> lastMovedTile) {
       moves = generateMoves(board, {-1, -1});
       if(moves.empty()) return {0,0,0}; 
   }
+
+  sortMoves(moves, board); // Sort the moves before evaluation
 
   int bestScore = INT_MAX;
   Move bestMove = moves[0];
